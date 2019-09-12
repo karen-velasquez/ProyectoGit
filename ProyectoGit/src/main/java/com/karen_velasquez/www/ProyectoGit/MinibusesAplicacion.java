8cc0f8c48085977b8c12473ffdb5d769fcee8ce7
karen-velasquez <54156284+karen-velasquez@users.noreply.github.com>
2019-09-12 01:27:15 -0400

    commit_tree

---
+++ ProyectoGit/src/main/java/com/karen_velasquez/www/ProyectoGit/MinibusesAplicacion.java
@@ -0,0 +1,13 @@
+package com.karen_velasquez.www.ProyectoGit;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinibusesAplicacion {
+	public static void main(String... args) {
+	    SpringApplication.run(MinibusesAplicacion.class, args);
+	  }
+
+}
