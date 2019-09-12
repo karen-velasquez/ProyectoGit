ba952fc31c172e01408bd571f989f949bfca41c1
karen-velasquez <54156284+karen-velasquez@users.noreply.github.com>
2019-09-12 01:02:06 -0400

    second_commit

---
+++ ProyectoGit/src/main/java/com/karen_velasquez/www/ProyectoGit/Minibus_repositorio.java
@@ -0,0 +1,8 @@
+package com.karen_velasquez.www.ProyectoGit;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+interface EmployeeRepository extends JpaRepository<Minibus, Long> {
+
+}
+
